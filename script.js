342e1a156d8ad20df46f893f0572561a88244ba2
Ana Clara <164914557+claraasx@users.noreply.github.com>
2024-05-13 23:10:53 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
+import axios from 'axios';
 
+const options = {
+  method: 'GET',
+  url: 'https://sky-scanner3.p.rapidapi.com/get-config',
+  headers: {
+    'X-RapidAPI-Key': '5cbb8ac269msh54c95c0addee57ap16efcfjsn33f1b7277539',
+    'X-RapidAPI-Host': 'sky-scanner3.p.rapidapi.com'
+  }
+};
+
+try {
+	const response = await axios.request(options);
+	console.log(response.data);
+} catch (error) {
+	console.error(error);
+}
